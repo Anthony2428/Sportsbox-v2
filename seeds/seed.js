e61241dd19e4b7e8b5699fcdaea537349382f000
Ant <alopez0218.al@gmail.com>
2021-07-20 21:47:09 -0700

    (Seeding)

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,19 +1,19 @@
 const sequelize = require('../config/connection');
 const { MLBTeams, NBATeams, NFLTeams, NHLTeams } = require('../models');
 
-// const MLBteamsSeedData = require('./mlbTeamSeed.json');
-// const NBAteamsSeedData = require('./nbaTeamSeed.json');
+const MLBteamsSeedData = require('./mlbTeamSeed.json');
+const NBAteamsSeedData = require('./nbaTeamSeed.json');
 const NFLteamsSeedData = require('./nflTeamSeed.json');
-//const NHLteamsSeedData = require('./nhlTeamSeed.json');
+const NHLteamsSeedData = require('./nhlTeamSeed.json');
 
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: false });
 
-  // const MLBteams = await MLBTeams.bulkCreate(MLBteamsSeedData);
+  const MLBteams = await MLBTeams.bulkCreate(MLBteamsSeedData);
   const NFLteams = await NFLTeams.bulkCreate(NFLteamsSeedData);
-  //const NHLteams = await NHLTeams.bulkCreate(NHLteamsSeedData);
-  // const NBAteams = await NBATeams.bulkCreate(NBAteamsSeedData);
+  const NHLteams = await NHLTeams.bulkCreate(NHLteamsSeedData);
+  const NBAteams = await NBATeams.bulkCreate(NBAteamsSeedData);
 
   process.exit(1);
 };
