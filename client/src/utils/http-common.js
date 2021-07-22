875424b29c0ebeb9959ad5634678a37286988e91
Ant <alopez0218.al@gmail.com>
2021-07-21 19:14:32 -0700

    (Heroku)

--- client/src/utils/http-common.js
+++ client/src/utils/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://shrouded-hollows-93454.herokuapp.com/api",
+  baseURL: window.location.origin,
   headers: {
     "Content-type": "application/json"
   }
