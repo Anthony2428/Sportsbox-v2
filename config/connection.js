5c8130c488bc7eaff48f5c0d240b492610dd2f6e
Ant <alopez0218.al@gmail.com>
2021-07-21 18:57:37 -0700

    (Heroku)

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// const sequelize = process.env.JAWSDB_URL
-// ? new Sequelize(process.env.JAWSDB_URL)
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    // host: 'ik1eybdutgxsm0lo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    host: 'localhost',
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'ik1eybdutgxsm0lo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306
   });
